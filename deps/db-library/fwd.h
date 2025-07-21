b35559f92e464c41a01d20773dc0ff67bceb5c64
twostars <i.am.twostars@gmail.com>
2025-07-22 07:44:44 +1000

    Move db-library to submodule (Open-KO/OpenKO-db-library)

--- deps/db-library/fwd.h
+++
@@ -1,8 +0,0 @@
-﻿#pragma once
-
-namespace db
-{
-	class Connection;
-	class ConnectionManager;
-	struct DatasourceConfig;
-}
--- deps/db-library/pch.h
+++
@@ -1,7 +0,0 @@
-﻿#pragma once
-
-#include <chrono>
-#include <format>
-#include <memory>
-
-#include <nanodbc/nanodbc.h>
